4a835472782c5502c82b3eccf2621942c8ee23ef
Dominic Groth <dominic.groth@messerli.ch>
2019-11-06 14:43:00 +0100

    Implement builder

--- PgpWordList/PgpWordAccumulationBuilder.cs
+++ PgpWordList/PgpWordAccumulationBuilder.cs
@@ -1,19 +1,48 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Messerli.PgpWordList
 {
     public class PgpWordAccumulationBuilder
     {
+        public const string DefaultSeparator = "-";
+
+        private readonly List<byte> _byteList = new List<byte>();
+
+        private string _separator = DefaultSeparator;
+
         public PgpWordAccumulationBuilder SetSeparator(string separator)
-            => throw new NotImplementedException();
+        {
+            _separator = separator;
+            return this;
+        }
 
         public PgpWordAccumulationBuilder Add(byte @byte)
-            => throw new NotImplementedException();
+        {
+            _byteList.Add(@byte);
+            return this;
+        }
 
         public PgpWordAccumulationBuilder Add(byte[] byteArray)
-            => throw new NotImplementedException();
+        {
+            _byteList.AddRange(byteArray);
+            return this;
+        }
 
         public PgpWordAccumulation Build()
-            => throw new NotImplementedException();
+            => new PgpWordAccumulation(
+                string.Join(
+                    _separator,
+                    _byteList.Aggregate(new List<string>(), ByteToWord)));
+
+        private static List<string> ByteToWord(List<string> list, byte @byte)
+        {
+            list.Add(
+                list.Count % 2 == 0
+                    ? PgpWordList.MapEven(@byte)
+                    : PgpWordList.MapOdd(@byte));
+
+            return list;
+        }
     }
 }
