74970cf69e1f699e3ef94af6610ed6c8bac9c4e4
Mathias Fischler <mathias.fischler@messerli.ch>
2021-09-27 11:52:55 +0200

    Add ParseOrNone and test error cases

--- PgpWordList.Test/PgpWordAggregationParserTest.cs
+++ PgpWordList.Test/PgpWordAggregationParserTest.cs
@@ -1,18 +1,44 @@
-﻿using Xunit;
+﻿using System;
+using Funcky.Xunit;
+using Xunit;
 
 namespace Messerli.PgpWordList.Test
 {
     public class PgpWordAggregationParserTest
     {
+        private const string InvalidWord = "invalidword";
+
         [Theory]
         [ClassData(typeof(PgpWordListTestData))]
-        public void ParsesPgpWordAggregation(byte[] expectedPgpBytes, string separator, string pgpWordAggregation)
+        public void ParsesPgpWordAggregationCorrectly(byte[] expectedPgpBytes, string separator, string pgpWordAggregation)
         {
-            var pgpBytes =
-                new PgpWordAggregationParser(separator)
-                    .Parse(new PgpWordAggregation(pgpWordAggregation));
+            var pgpBytes = new PgpWordAggregationParser(separator)
+                .Parse(new PgpWordAggregation(pgpWordAggregation));
 
             Assert.Equal(expectedPgpBytes, pgpBytes);
         }
+
+        [Theory]
+        [ClassData(typeof(PgpWordListTestData))]
+        public void ParsesPgpWordAggregationIntoOptionCorrectly(byte[] expectedPgpBytes, string separator, string pgpWordAggregation)
+        {
+            var pgpBytes = new PgpWordAggregationParser(separator)
+                .ParseOrNone(new PgpWordAggregation(pgpWordAggregation));
+
+            Assert.Equal(expectedPgpBytes, FunctionalAssert.IsSome(pgpBytes));
+        }
+
+        [Fact]
+        public void ThrowsExceptionWhenParsingInvalidPgpWords()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new PgpWordAggregationParser()
+                .Parse(new PgpWordAggregation($"{InvalidWord}-adroitness-aardvark-adroitness")));
+            Assert.Equal($"'{InvalidWord}' is an invalid pgp word.", exception.Message);
+        }
+
+        [Fact]
+        public void ReturnsNoneWhenParsingInvalidPgpWords()
+            => FunctionalAssert.IsNone(new PgpWordAggregationParser()
+                .ParseOrNone(new PgpWordAggregation($"{InvalidWord}-adroitness-aardvark-adroitness")));
     }
 }
--- PgpWordList/PgpWordAggregationParser.cs
+++ PgpWordList/PgpWordAggregationParser.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using Funcky.Extensions;
+using Funcky.Monads;
+using static Funcky.Functional;
 
 namespace Messerli.PgpWordList
 {
@@ -15,14 +18,22 @@ namespace Messerli.PgpWordList
         }
 
         public byte[] Parse(PgpWordAggregation pgpWordAggregation)
+            => ParseInternal(pgpWordAggregation)
+                .Match(
+                    left: word => throw new ArgumentException($"'{word}' is an invalid pgp word."),
+                    right: Identity);
+
+        public Option<byte[]> ParseOrNone(PgpWordAggregation pgpWordAggregation)
+            => ParseInternal(pgpWordAggregation).Match(_ => Option<byte[]>.None(), Option.Some);
+
+        private Either<string, byte[]> ParseInternal(PgpWordAggregation pgpWordAggregation)
             => pgpWordAggregation
                 .Value
                 .Split(new[] { _separator }, StringSplitOptions.None)
-                .Select(WordToByte)
-                .ToArray();
+                .SelectWhileSome(WordToByte);
 
-        private static byte WordToByte(string word, int index)
-            => WordToByte(word, IsEven(index)) ?? throw new ArgumentException($"'{word}' is an invalid pgp word.");
+        private static Option<byte> WordToByte(string word, int index)
+            => Option.FromNullable(WordToByte(word, IsEven(index)));
 
         private static byte? WordToByte(string word, bool even)
             => even
@@ -30,5 +41,22 @@ namespace Messerli.PgpWordList
                 : PgpWordList.FindOdd(word);
 
         private static bool IsEven(int number) => number % 2 == 0;
+    }
+
+    internal static class PgpWordArrayExtensions
+    {
+        public static Either<string, byte[]> SelectWhileSome(this string[] elements, Func<string, int, Option<byte>> selector)
+            => elements.WithIndex().Aggregate(
+                seed: Either<string, byte[]>.Right(new byte[elements.Length]),
+                (accumulator, element)
+                    => accumulator.Match(
+                        left: Either<string, byte[]>.Left,
+                        right: bytes => selector(element.Value, element.Index).Match(
+                            some: @byte =>
+                            {
+                                bytes[element.Index] = @byte;
+                                return Either<string, byte[]>.Right(bytes);
+                            },
+                            none: Either<string, byte[]>.Left(element.Value))));
     }
 }
