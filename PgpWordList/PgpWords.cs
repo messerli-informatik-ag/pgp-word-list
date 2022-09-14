0bbe2cf2b0cc521825592064c93c5fcdd69df0f4
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-14 14:41:57 +0200

    Use list instead of dictionary

--- PgpWordList/PgpWords.cs
+++ PgpWordList/PgpWords.cs
@@ -1,21 +1,24 @@
-using Funcky.Extensions;
+using System;
 using Funcky.Monads;
 
 namespace Messerli.PgpWordList;
 
 public static partial class PgpWords
 {
+    private const int NotFoundIndex = -1;
+
     public static string ToEvenWord(byte @byte) => Data[@byte].Even;
 
     public static string ToOddWord(byte @byte) => Data[@byte].Odd;
 
     public static Option<byte> ToEvenByteOrNone(string word)
-        => Data
-            .FirstOrNone(pair => pair.Value.Even == word)
-            .Select(pair => pair.Key);
+        => ToByte(pair => pair.Even == word);
 
     public static Option<byte> ToOddByteOrNone(string word)
-        => Data
-            .FirstOrNone(pair => pair.Value.Odd == word)
-            .Select(pair => pair.Key);
+        => ToByte(pair => pair.Odd == word);
+
+    private static Option<byte> ToByte(Predicate<(string Even, string Odd)> predicate)
+        => Option.Return(Array.FindIndex(Data, predicate))
+            .Where(index => index != NotFoundIndex)
+            .Select(Convert.ToByte);
 }
